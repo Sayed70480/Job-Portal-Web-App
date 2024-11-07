4fdcfd0f300d15d6b61841416e92f631a47cfc40
Syed Sikandar Shah <88287750+Sayed70480@users.noreply.github.com>
2024-11-07 10:01:11 +0500

    Update app.js

--- Server/app.js
+++ Server/app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "https://job-portal-web-app-qnhl.vercel.app/",
+  origin: "https://job-portal-web-app-qnhl.vercel.app",
   credentials: true,
 };
 
