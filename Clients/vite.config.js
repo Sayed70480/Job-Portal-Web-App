ad6fb3277c1c5bec6c302e6cb357119268382b2c
Sayed70480 <syedsiknadars4@gmail.com>
2024-11-11 11:11:37 +0500

    add

--- Clients/vite.config.js
+++ Clients/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  build: {
+    outDir: 'dist', // Ensure this is `dist` or your desired folder
+  },
   plugins: [react()],
   resolve: {
     alias: {
